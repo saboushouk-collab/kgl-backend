526b3ac460c07470f7e406cd8d7fb3258bd03e83
SAMI MAHGOUB <saboushouk@student.refactory.academy>
2026-02-18 20:36:05 +0300

    Initialize Repository

---
+++ src/README.md
@@ -0,0 +1,19 @@
+# KGL Backend API
+
+Backend system for managing:
+- Users
+- Procurement
+- Sales
+
+Built with:
+- Node.js
+- Express
+- MongoDB
+- Swagger
+
+---
+
+## Installation
+
+Clone the project:
+
--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI);
-        console.log("MongoDB Connected");
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.DATABASE_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Error:", error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
